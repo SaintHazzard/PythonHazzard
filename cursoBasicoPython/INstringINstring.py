ae92b8dd5dffea2d9321395e4c2d3306bd224c21
SaintHazzard <chamitos1998@hotmail.com>
2022-09-13 17:18:37 -0500

    MAP y INstringINstring

---
+++ cursoBasicoPython/INstringINstring.py
@@ -0,0 +1,22 @@
+def get_count(sentence):
+    contador = 0
+    for letra in sentence:
+        if letra.lower() in 'aeiou':
+            contador += 1
+
+    return contador
+
+
+
+
+def digitize(n):
+    return [int(x) for x in str(n)[::-1]]
+    
+    
+    
+#  n = str(n)
+#     print(list(n[::-1]))
+#     for val in n:
+#         list(val)   
+
+print(digitize(n))
---
+++ cursoBasicoPython/mapIMPORTANT.py
@@ -0,0 +1,4 @@
+n = 35231
+print(map(int,str(n)))
+
+# Map ejecuta la funcion como primer argumento y como segundo argumento toma el iterable IMPORTANT
